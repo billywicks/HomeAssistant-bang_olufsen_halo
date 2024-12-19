7c11ff8d2889157f9e6dcc92ad7eb19ac230992c
Billy Wicks <84421603+billywicks@users.noreply.github.com>
2024-12-19 09:19:29 +0000

    Delete bang_olufsen_halo/bang_olufsen_halo directory

--- bang_olufsen_halo/bang_olufsen_halo/const.py
+++
@@ -1,6 +0,0 @@
-DOMAIN = "bang_olufsen_halo"
-CONF_HOST = "host"
-CONF_PORT = "port"
-EVENT_TYPE_IN = "bang_olufsen_halo_websocket_event_in"
-EVENT_TYPE_OUT = "bang_olufsen_halo_websocket_event_out"
-CONF_JSON_DATA = "json_data"
